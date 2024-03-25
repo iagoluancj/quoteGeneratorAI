7611e27a18494f50535601bd4d9494165f083466
Iago Luan <iago.luancj@gmail.com>
2024-03-24 23:08:52 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,63 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import { useChat } from 'ai/react';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { GrLinkedinOption } from "react-icons/gr";
+import { GrInstagram } from "react-icons/gr";
+import { GrGithub } from "react-icons/gr";
+
+export default function Chat() {
+  const { messages, handleInputChange, handleSubmit } = useChat();
+
+  const createNewCitation = () => {
+    const event = { target: { value: 'Crie uma citação falsa de um autor falso (observação: não diga que o autor é falso, siga modelo comum de citação: "frase - autor", o autor deve conter um nome coerente (Evite nomes com substantivos utilizados na frase gerada))' } };
+    handleInputChange(event);
+  }
+
+  const lastAssistantMessage = messages
+    .filter(m => m.role === 'assistant')
+    .pop();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="flex flex-col w-full max-w-md mx-auto justify-center pt-28 text-center">
+        <div
+          className='bg-violet-50 rounded-lg flex flex-col m-3'
+        >
+          {lastAssistantMessage ? (
+            <div key={lastAssistantMessage.id} className="whitespace-pre-wrap text-violet-700 px-4 py-5 text-xl">
+              <span>{lastAssistantMessage.content}</span>
+            </div>
+          ) : (
+            <div className="whitespace-pre-wrap text-violet-700 px-4 py-5">
+              <span>Crie citações únicas e exclusivas geradas a partir da integração API Vercel com LangChain, prossiga pelo botão abaixo - Iago Jesus</span>
+            </div>
+          )}
+          <div className='flex gap-3 justify-evenly py-3 text-violet-500'>
+            <div><a href="https://www.linkedin.com/in/iago-luan-4b4b02220/" target='_blank' aria-label='Icone Linkedin'><GrLinkedinOption size={24} /> </a></div>
+            <div><a href="https://www.instagram.com/iago.luancj/" target='_blank' aria-label='Icone Instagram'><GrInstagram size={24} /></a></div>
+            <div><a href="https://github.com/iagoluancj" target='_blank' aria-label='Icone Github'><GrGithub size={24} /></a></div>
+          </div>
         </div>
+        <span className='pt-1 text-violet-200'>
+          Developed by <a href="https://iagoluancj.github.io/Portifolio-React/" target='_blank' className='underline'>Iago Jesus</a>
+        </span>
+        <form
+          className="mt-10 px-3"
+          onSubmit={handleSubmit}>
+          <button
+            className="w-full
+          bg-violet-50 border
+          border-violet-300 
+          rounded-md shadow-xl py-2 text-violet-700 
+          hover:bg-violet-400 hover:text-white md:hover:bg-violet-50 md:hover:text-violet-700 md:active:bg-violet-400 md:active:text-white ease-in-out duration-200"
+            onClick={createNewCitation}
+          >
+            Criar nova citação
+          </button>
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
