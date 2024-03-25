61cea26cc2d3606dcddd3ed95e814302866e6d7d
Iago Luan <iago.luancj@gmail.com>
2024-03-24 23:21:21 -0300

    deleted env

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -23,12 +23,6 @@ Current conversation:
 User: {input}
 AI:`;
  
-/*
- * This handler initializes and calls a simple chain with a prompt,
- * chat model, and output parser. See the docs for more information:
- *
- * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
- */
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const messages = body.messages ?? [];
@@ -36,26 +30,13 @@ export async function POST(req: NextRequest) {
   const currentMessageContent = messages[messages.length - 1].content;
  
   const prompt = PromptTemplate.fromTemplate(TEMPLATE);
-  /**
-   * See a full list of supported models at:
-   * https://js.langchain.com/docs/modules/model_io/models/
-   */
+
   const model = new ChatOpenAI({
     temperature: 0.8,
   });
  
-  /**
-   * Chat models stream message chunks rather than bytes, so this
-   * output parser handles serialization and encoding.
-   */
   const outputParser = new BytesOutputParser();
  
-  /*
-   * Can also initialize as:
-   *
-   * import { RunnableSequence } from "langchain/schema/runnable";
-   * const chain = RunnableSequence.from([prompt, model, outputParser]);
-   */
   const chain = prompt.pipe(model).pipe(outputParser);
  
   const stream = await chain.stream({
